33c2811c82a67237223cec30278e2c1397e41668
k.steinmetz <karel.steinmetz@gmail.com>
2016-07-07 15:57:01 +0200

    Added columnsCount.

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import * as b from "bobril";
 
 b.asset("./css/flexboxgrid.min.css");
 
+export const columnsCount = 12;
+
 export * from "./src/grid";
 export * from "./src/row";
 export * from "./src/col";
